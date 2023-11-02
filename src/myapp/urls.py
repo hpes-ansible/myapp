0136037ad8014ab454a4450b1ab284089fc8abfc
砂川 延昭 <sunagawa.nobuaki@edifist.co.jp>
2023-11-02 14:04:17 +0900

    url path change

--- src/myapp/urls.py
+++ src/myapp/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url, include
+from django.conf.urls import path, include
 
 urlpatterns = [
-    url(r'^', include('polls.urls')),
-    url(r'^host/', 'host.views.host'),
+    path(r'^', include('polls.urls')),
+    path(r'^host/', 'host.views.host'),
 ]
