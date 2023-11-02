092f2a9be7a41f0b8d71598effd69beb685fd339
砂川 延昭 <sunagawa.nobuaki@edifist.co.jp>
2023-11-02 15:31:21 +0900

    change

--- src/myapp/settings.py
+++ src/myapp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'host',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
